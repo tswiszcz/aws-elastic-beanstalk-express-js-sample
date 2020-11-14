8a218c288e90be0b1ffbde52af3d97be4727fcc4
tswisz2 <tswisz2@illinois.edu>
2020-11-14 11:42:41 -0600

    Updated display text

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Jeff B. You son of a bitch!!'));
+app.get('/', (req, res) => res.send('Thomas Swiszcz is cool like'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
